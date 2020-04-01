8733a3c04b55d1ebaa9135022114f8e0c6c30b01
jonesjn6 <62968398+jonesjn6@users.noreply.github.com>
2020-04-01 17:01:18 -0400

    Add files via upload

---
+++ Window.java
@@ -0,0 +1,50 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.widgets.Menu;
+
+public class Window {
+
+	protected Shell shlVirtue;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Window window = new Window();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shlVirtue.open();
+		shlVirtue.layout();
+		while (!shlVirtue.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shlVirtue = new Shell();
+		shlVirtue.setSize(626, 415);
+		shlVirtue.setText("VirtuBase Games");
+
+	}
+}
