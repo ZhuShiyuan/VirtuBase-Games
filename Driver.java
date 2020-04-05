325c493d4d3708ba5111529c465c706353102e84
byrnejr <62897152+byrnejr@users.noreply.github.com>
2020-04-05 14:55:44 -0400

    Update Driver.java
    GameData read from file

--- Driver.java
+++ Driver.java
@@ -4,112 +4,122 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.net.URL;
-import java.net.URLConnection;
+import java.net.MalformedURLException;
 
 public class Driver {
-
-//	public static void main(String[] args) {
-//		writeSortedToFile();
-//		writeProfitsToFile();
-//	}
 	
-	public static KeywordIndex retrieveKeywordIndex() {
+	public static KeywordIndex keyDex;
+	public static ArrayList<GameData> gameList;
+
+	public static void main(String[] args) {
 		try {
-			KeywordIndex newIndex = new KeywordIndex();
-			Scanner input = new Scanner(new File("keywordIndex.csv"));
-			String[] stringList = input.nextLine().split(",");
-			newIndex.readList(stringList);
+			Scanner input = new Scanner(new File("database.csv"));
+			retrieveKeywordIndex(input);
+			retrieveGamesList(input);
 			input.close();
-			return newIndex;
 		} catch (FileNotFoundException e) {
 			System.out.println("FILE NOT FOUND");
-			return null;
 		}
 	}
 	
-// -----------------------------------------------------------------
+	public static void retrieveKeywordIndex(Scanner input) {
+		String[] stringList = input.nextLine().split(",");
+		keyDex.readList(stringList);
+	}
 	
 	// Reads the data from the file and puts it into an sorted array
-	public static GameData[] retrieveGamesList() {
-
-		ArrayList<GameData> gameDataAL = new ArrayList<GameData>();
-
-		try {
-			Scanner input = new Scanner(new File("gamedata.csv"));
-			while (input.hasNext()) {
-				String[] currentString = input.nextLine().split(",");
-				gameDataAL.add(new GameData(currentString[1], currentString[2], Integer.parseInt(currentString[3]),
-						Double.parseDouble(currentString[6])));
+	public static void retrieveGamesList(Scanner input) {
+		
+		while (input.hasNext()) {
+			String[] data = input.nextLine().split(",");
+			
+			String title = data[0];
+			String company = data[1];
+			URL wiki;
+			try { wiki = new URL(data[2]); } catch (MalformedURLException e) {
+				System.out.println("MALFORMED URL FOR " + data[2]); 
+				wiki = null;
 			}
-			GameData[] gameData = gameDataAL.toArray(new GameData[0]);
-			quickSort(gameData, 0, gameData.length - 1);
-			return gameData;
-		} catch (FileNotFoundException e) {
-			System.out.println("FILE NOT FOUND");
-			return null;
-		}
-	}
-
-	// partitions the array list of games
-	public static int partition(GameData[] gameData, int start, int end) {
-		GameData pivot = gameData[end];
-		int swapLocation = start;
-
-		for (int i = start; i < end; i++) {
-			if (gameData[i].compareTo(pivot) < 0) {
-				GameData temp = gameData[i];
-				gameData[i] = gameData[swapLocation];
-				gameData[swapLocation] = temp;
-				swapLocation++;
+			int[] keywords = new int[] {Integer.parseInt(data[3]), Integer.parseInt(data[4]), Integer.parseInt(data[5])};
+			ArrayList<Storefront> storeList = new ArrayList<Storefront>();
+			for (int i = 6; i < data.length; i+=3) {
+				URL storeURL;
+				try { storeURL = new URL(data[i+2]); } catch (MalformedURLException e) {
+					System.out.println("MALFORMED URL FOR " + data[i]); 
+					storeURL = null;
+				}
+				storeList.add(new Storefront(data[i],Double.parseDouble(data[i+1]),storeURL));
 			}
-		}
-
-		gameData[end] = gameData[swapLocation];
-		gameData[swapLocation] = pivot;
-		return swapLocation;
-
-	}
-
-	// Sorts the data from the file into a sorted arrayList of games
-	public static void quickSort(GameData[] gameData, int start, int end) {
-
-		if (start >= end) {
-			return;
-		} else {
-			int pivotLocation = partition(gameData, start, end);
-			quickSort(gameData, start, pivotLocation - 1);
-			quickSort(gameData, pivotLocation + 1, end);
-		}
-
-	}
-
-	// Writes the data to the sorted.csv file
-	public static void writeSortedToFile() {
-		try {
-			GameData[] gameData = retrieveGamesList();
-			PrintWriter writer = new PrintWriter("sorted.csv");
-			for (int i = 0; i < gameData.length; i++) {
-				writer.println(gameData[i].getTitle() + "," + gameData[i].getConsole() + "," + gameData[i].getYear()
-						+ "," + gameData[i].getSales());
+			gameList.add(new GameData(
+					title,
+					company, 
+					wiki, 
+					keywords, 
+					storeList,
+					keyDex));
 			}
-			writer.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("FILE NOT FOUND");
-		}
 	}
 
-	// Writes the data to the profits.csv file
-	public static void writeProfitsToFile() {
-		try {
-			ConsoleData[] consoleData = countMoney();
-			PrintWriter writer = new PrintWriter("profits.csv");
-			for (int i = 0; i < consoleData.length; i++) {
-				writer.println(consoleData[i].getConsole() + "," + consoleData[i].getSales());
-			}
-			writer.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("FILE NOT FOUND");
-		}
-	}
+//	// partitions the array list of games
+//	public static int partition(GameData[] gameData, int start, int end) {
+//		GameData pivot = gameData[end];
+//		int swapLocation = start;
+//
+//		for (int i = start; i < end; i++) {
+//			if (gameData[i].compareTo(pivot) < 0) {
+//				GameData temp = gameData[i];
+//				gameData[i] = gameData[swapLocation];
+//				gameData[swapLocation] = temp;
+//				swapLocation++;
+//			}
+//		}
+//
+//		gameData[end] = gameData[swapLocation];
+//		gameData[swapLocation] = pivot;
+//		return swapLocation;
+//
+//	}
+//
+//	// Sorts the data from the file into a sorted arrayList of games
+//	public static void quickSort(GameData[] gameData, int start, int end) {
+//
+//		if (start >= end) {
+//			return;
+//		} else {
+//			int pivotLocation = partition(gameData, start, end);
+//			quickSort(gameData, start, pivotLocation - 1);
+//			quickSort(gameData, pivotLocation + 1, end);
+//		}
+//
+//	}
+//
+//	// Writes the data to the sorted.csv file
+//	public static void writeSortedToFile() {
+//		try {
+//			GameData[] gameData = retrieveGamesList();
+//			PrintWriter writer = new PrintWriter("sorted.csv");
+//			for (int i = 0; i < gameData.length; i++) {
+//				writer.println(gameData[i].getTitle() + "," + gameData[i].getConsole() + "," + gameData[i].getYear()
+//						+ "," + gameData[i].getSales());
+//			}
+//			writer.close();
+//		} catch (FileNotFoundException e) {
+//			System.out.println("FILE NOT FOUND");
+//		}
+//	}
+//
+//	// Writes the data to the profits.csv file
+//	public static void writeProfitsToFile() {
+//		try {
+//			ConsoleData[] consoleData = countMoney();
+//			PrintWriter writer = new PrintWriter("profits.csv");
+//			for (int i = 0; i < consoleData.length; i++) {
+//				writer.println(consoleData[i].getConsole() + "," + consoleData[i].getSales());
+//			}
+//			writer.close();
+//		} catch (FileNotFoundException e) {
+//			System.out.println("FILE NOT FOUND");
+//		}
+//	}
 
 }
