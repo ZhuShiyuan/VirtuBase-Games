6354ef0146dc8b8753eb69e7bcc51498d6f1b782
byrnejr <62897152+byrnejr@users.noreply.github.com>
2020-04-15 14:05:30 -0400

    Driver File Update
    Read/ Write to file is working, and some method testing has been done. Make sure you don't have database.csv open when reading and writing, will be unable to find it.

--- Driver.java
+++ Driver.java
@@ -8,119 +8,87 @@ import java.net.MalformedURLException;
 
 public class Driver {
 	
-	public static KeywordIndex keyDex;
-	public static ArrayList<GameData> gameList;
+	public static KeywordIndex keyDex = new KeywordIndex();
+	public static ArrayList<GameData> gameList = new ArrayList<GameData>();
 
 	public static void main(String[] args) {
+		retrieveGamesList();
+		dataTest(gameList.get(0));
+		writeToFile();
+	}
+	
+	// Read data from database file
+	public static void retrieveGamesList() {
+		
 		try {
 			Scanner input = new Scanner(new File("database.csv"));
-			//retrieveKeywordIndex(input);
-			retrieveGamesList(input);
+			String[] stringList = input.nextLine().split(",");
+			keyDex.readList(stringList);
+			
+			while (input.hasNext()) {
+				String[] data = input.nextLine().split(",");
+				
+				String title = data[0];
+				String company = data[1];
+				URL wiki;
+				try { wiki = new URL(data[2]); } catch (MalformedURLException e) {
+					System.out.println("MALFORMED URL FOR " + data[2]); 
+					wiki = null;
+				}
+				int[] keywords = new int[] {Integer.parseInt(data[3]), Integer.parseInt(data[4]), Integer.parseInt(data[5])};
+				ArrayList<Storefront> storeList = new ArrayList<Storefront>();
+				for (int i = 6; i < data.length; i+=3) {
+					URL storeURL;
+					try { storeURL = new URL(data[i+2]); } catch (MalformedURLException e) {
+						System.out.println("MALFORMED URL FOR " + data[i]); 
+						storeURL = null;
+					}
+					storeList.add(new Storefront(data[i],Double.parseDouble(data[i+1]),storeURL));
+				}
+				gameList.add(new GameData(
+						title,
+						company, 
+						wiki, 
+						keywords, 
+						storeList,
+						keyDex));
+				}
 			input.close();
 		} catch (FileNotFoundException e) {
-			System.out.println("FILE NOT FOUND");
+			System.out.println("READ FILE NOT FOUND");
 		}
 	}
 	
-	public static void retrieveKeywordIndex(Scanner input) {
-		String[] stringList = input.nextLine().split(",");
-		keyDex.readList(stringList);
+	// Write data to database file
+	public static void writeToFile() {
+		try {
+			PrintWriter writer = new PrintWriter("database.csv");
+			String[] keywords = keyDex.writeList();
+			String formatKeyword = "";
+			for (String i :keywords) {
+				formatKeyword = formatKeyword + i + ",";
+			}
+			writer.println(formatKeyword);
+			for (int i=0; i<gameList.size();i++) {
+				writer.println(gameList.get(i).prepForFile());
+			}
+			writer.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("WRITE FILE NOT FOUND");
+		}
 	}
 	
-	// Reads the data from the file and puts it into an sorted array
-	public static void retrieveGamesList(Scanner input) {
-		
-		while (input.hasNext()) {
-			String[] data = input.nextLine().split(",");
-			
-			String title = data[0];
-			String company = data[1];
-			URL wiki;
-			try { wiki = new URL(data[2]); } catch (MalformedURLException e) {
-				System.out.println("MALFORMED URL FOR " + data[2]); 
-				wiki = null;
-			}
-			int[] keywords = new int[] {Integer.parseInt(data[3]), Integer.parseInt(data[4]), Integer.parseInt(data[5])};
-			ArrayList<Storefront> storeList = new ArrayList<Storefront>();
-			for (int i = 6; i < data.length; i+=3) {
-				URL storeURL;
-				try { storeURL = new URL(data[i+2]); } catch (MalformedURLException e) {
-					System.out.println("MALFORMED URL FOR " + data[i]); 
-					storeURL = null;
-				}
-				storeList.add(new Storefront(data[i],Double.parseDouble(data[i+1]),storeURL));
-			}
-			gameList.add(new GameData(
-					title,
-					company, 
-					wiki, 
-					keywords, 
-					storeList,
-					keyDex));
-			}
+	// Tester method, prints data members of chosen game and first listed storefront
+	public static void dataTest(GameData game) {
+		System.out.println(game.getTitle());
+		System.out.println(game.getCompany());
+		System.out.println(game.getWiki());
+		System.out.println(game.getStorefront(0).getName());
+		System.out.println(game.getStorefront(0).getPrice());
+		System.out.println(game.getStorefront(0).getLink());
+		System.out.println(game.getKeyword(0));
+		System.out.println(game.getKeyword(1));
+		System.out.println(game.getKeyword(2));
 	}
 
-//	// partitions the array list of games
-//	public static int partition(GameData[] gameData, int start, int end) {
-//		GameData pivot = gameData[end];
-//		int swapLocation = start;
-//
-//		for (int i = start; i < end; i++) {
-//			if (gameData[i].compareTo(pivot) < 0) {
-//				GameData temp = gameData[i];
-//				gameData[i] = gameData[swapLocation];
-//				gameData[swapLocation] = temp;
-//				swapLocation++;
-//			}
-//		}
-//
-//		gameData[end] = gameData[swapLocation];
-//		gameData[swapLocation] = pivot;
-//		return swapLocation;
-//
-//	}
-//
-//	// Sorts the data from the file into a sorted arrayList of games
-//	public static void quickSort(GameData[] gameData, int start, int end) {
-//
-//		if (start >= end) {
-//			return;
-//		} else {
-//			int pivotLocation = partition(gameData, start, end);
-//			quickSort(gameData, start, pivotLocation - 1);
-//			quickSort(gameData, pivotLocation + 1, end);
-//		}
-//
-//	}
-//
-//	// Writes the data to the sorted.csv file
-//	public static void writeSortedToFile() {
-//		try {
-//			GameData[] gameData = retrieveGamesList();
-//			PrintWriter writer = new PrintWriter("sorted.csv");
-//			for (int i = 0; i < gameData.length; i++) {
-//				writer.println(gameData[i].getTitle() + "," + gameData[i].getConsole() + "," + gameData[i].getYear()
-//						+ "," + gameData[i].getSales());
-//			}
-//			writer.close();
-//		} catch (FileNotFoundException e) {
-//			System.out.println("FILE NOT FOUND");
-//		}
-//	}
-//
-//	// Writes the data to the profits.csv file
-//	public static void writeProfitsToFile() {
-//		try {
-//			ConsoleData[] consoleData = countMoney();
-//			PrintWriter writer = new PrintWriter("profits.csv");
-//			for (int i = 0; i < consoleData.length; i++) {
-//				writer.println(consoleData[i].getConsole() + "," + consoleData[i].getSales());
-//			}
-//			writer.close();
-//		} catch (FileNotFoundException e) {
-//			System.out.println("FILE NOT FOUND");
-//		}
-//	}
-
 }
-
