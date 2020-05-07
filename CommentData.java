983b1d45692ff3c5c435cb442b9cf708723df475
byrnejr <62897152+byrnejr@users.noreply.github.com>
2020-05-07 11:29:40 -0400

    Javadoc Comments

--- CommentData.java
+++ CommentData.java
@@ -3,26 +3,40 @@ public class CommentData {
 	
 	private String game, username, comment;
 	
+	/**
+	 * Generate a new CommentData.
+	 * @param gameName - Name of the game being commented.
+	 * @param user - Username that is writing the comment.
+	 * @param commentSelection - The comment being written.
+	 */
 	public CommentData(String gameName, String user, String commentSelection) {
 		this.game = gameName;
 		this.username = user;
 		this.comment = commentSelection;
 	}
 	
+	/**
+	 * Returns the title of comment's game.
+	 * @return title of the game.
+	 */
 	public String getGame() {
 		return this.game;
 	}
 	
+	/**
+	 * Returns the username that wrote the comment.
+	 * @return username of the comment.
+	 */
 	public String getUsername() {
 		return this.username;
 	}
 	
+	/**
+	 * Returns the content of the comment.
+	 * @return comment content.
+	 */
 	public String getComment() {
 		return this.comment;
-	}
-
-	public static void add(String game2, String username2, String comment2) {
-		
 	}
 
 }
