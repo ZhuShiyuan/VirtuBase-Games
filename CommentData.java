c8813633128fcfd10ae782f9215c2e4ee62d7e44
byrnejr <62897152+byrnejr@users.noreply.github.com>
2020-05-08 16:06:12 -0400

    Javadoc Comments

--- CommentData.java
+++ CommentData.java
@@ -3,25 +3,47 @@ public class CommentData {
 	
 	private String game, username, comment;
 	
+	/**
+	 * Generate a new CommentData.
+	 * @param gameName - Name of the game being commented.
+	 * @param user - Username that is writing the comment.
+	 * @param commentSelection - The comment being written.
+	 */
 	public CommentData(String gameName, String user, String commentSelection) {
 		this.game = gameName;
 		this.username = user;
 		this.comment = commentSelection;
 	}
 	
+	
+	/**
+	 * Returns the title of comment's game.
+	 * @return title of the game.
+	 */
 	public String getGame() {
 		return this.game;
 	}
 	
+	/**
+	 * Returns the username that wrote the comment.
+	 * @return username of the comment.
+	 */
 	public String getUsername() {
 		return this.username;
 	}
 	
+	/**
+	 * Returns the content of the comment.
+	 * @return comment content.
+	 */
 	public String getComment() {
 		return this.comment;
 	}
 	
-	public void deleteGame() {
+	/**
+	 * Flag this comment as deleted and remove it from the game's listed comments.
+	 */
+	public void deleteComment() {
 		this.game = "DELETED";
 		Driver.writeToFile();
 	}
